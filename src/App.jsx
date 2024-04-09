510599d052770c180eeb9361c60eae66961b7f19
ashishshukla9 <shuklaashish655@gmail.com>
2024-04-09 17:36:52 +0530

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
 import Services from './Components/Services/Services'
 import MyWork from './Components/MyWork/MyWork'
+import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
 const App = () => {
   return (
     <div>
@@ -12,6 +14,8 @@ const App = () => {
       <About />
       <Services />
       <MyWork />
+      <Contact />
+      <Footer />
     </div>
   )
 }
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import './Hero.css'
 import profile_img from '../../assets/profile_img.svg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 const Hero = () => {
     return (
-        <div className='hero'>
+        <div id='home' className='hero'>
             <img src={profile_img} alt="" />
             <h1><span>I'm Ashish,</span> Frontend developer based in India.</h1>
             <p>I am a frontend developer rom mumbai, india with 08 years of experiance</p>
             <div className='hero-action'>
-                <div className='hero-connect'>Connect With Us</div>
+                <div className='hero-connect'><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Us</AnchorLink></div>
                 <div className='hero-resume'>My Resume</div>
             </div>
         </div>
